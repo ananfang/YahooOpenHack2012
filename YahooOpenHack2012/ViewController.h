9f3923f61d4a9645423b8df9593dc8b7ea611faa
ananfang <ananfang@gmail.com>
2012-10-20 12:57:07 +0800

    Initial Commit

---
+++ YahooOpenHack2012/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YahooOpenHack2012
+//
+//  Created by Fang Yung-An on 12/10/20.
+//  Copyright (c) 2012年 Openmouse Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ YahooOpenHack2012/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YahooOpenHack2012
+//
+//  Created by Fang Yung-An on 12/10/20.
+//  Copyright (c) 2012年 Openmouse Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
